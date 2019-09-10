6c34d07591f08359b89bbd22e1c8956d9e96ef11
GeorgescuC <GeorgescuC@users.noreply.github.com>
2019-09-10 16:15:03 -0400

    Merge remote-tracking branch 'origin/devel'

--- R/inferCNV_ops.R
+++ R/inferCNV_ops.R
@@ -1059,6 +1059,12 @@ run <- function(infercnv_obj,
                          png_res=png_res
                          )
             }
+            ## write the adjusted CNV report files
+            ## report predicted cnv regions:
+            adjust_genes_regions_report(mcmc_obj_hmm_states_list[[1]],
+                                        input_filename_prefix=sprintf("%02d_HMM_preds", (step_count-1)),
+                                        output_filename_prefix=sprintf("HMM_CNV_predictions.%s.Pnorm_%g", hmm_resume_file_token, BayesMaxPNormal),
+                                        out_dir=out_dir)
         }
         
         
@@ -1098,15 +1104,6 @@ run <- function(infercnv_obj,
                          png_res=png_res
                          )
             }
-            
-            ## write the adjusted CNV report files
-            ## report predicted cnv regions:
-            generate_cnv_region_reports(hmm.infercnv_obj,
-                                        output_filename_prefix=sprintf("HMM_CNV_predictions.%s.Pnorm_%g", hmm_resume_file_token, BayesMaxPNormal),
-                                        out_dir=out_dir,
-                                        ignore_neutral_state=1,
-                                        by=HMM_report_by)
-            
         }
     }
     
