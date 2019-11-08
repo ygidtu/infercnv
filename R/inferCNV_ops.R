1f75be50ef3d345e7888b0961fbf95eb278dfc1f
GeorgescuC <GeorgescuC@users.noreply.github.com>
2019-11-08 14:02:22 -0500

    Fix _R_CHECK_LENGTH_1_LOGIC2_ related error because an argument was checked before matching to it's list of potential arguments.

--- R/inferCNV_ops.R
+++ R/inferCNV_ops.R
@@ -294,6 +294,7 @@ run <- function(infercnv_obj,
 
 
     smooth_method = match.arg(smooth_method)
+    HMM_type = match.arg(HMM_type)
     if (HMM && HMM_type == 'i6' && smooth_method == 'coordinates') {
         flog.error("Cannot use 'coordinate' smoothing method with i6 HMM model at this time.")
         stop("Incompatible HMM mode and smoothing method.")
@@ -321,7 +322,6 @@ run <- function(infercnv_obj,
         HMM_report_by = "cells"
     }
     tumor_subcluster_partition_method = match.arg(tumor_subcluster_partition_method)
-    HMM_type = match.arg(HMM_type)
     
     if (debug) {
         flog.threshold(DEBUG)
